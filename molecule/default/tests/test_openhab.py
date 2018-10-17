8ad6ad6075f1cc867b6882892eae05012d9d7555
Krzysztof <krzysztof_rrr@wp.pl>
2018-10-17 12:20:31 +0200

    Initial molecule setup for vagrant and first tests

---
+++ molecule/default/tests/test_openhab.py
@@ -0,0 +1,16 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('openhab')
+
+
+def test_apt_transport_https_installed(host):
+    package = host.package("apt-transport-https")
+    assert package.is_installed
+
+
+def test_openhab_2_2_0_is_installed(host):
+    assert host.package('openhab2').is_installed
+    assert host.package('openhab2-addons').is_installed

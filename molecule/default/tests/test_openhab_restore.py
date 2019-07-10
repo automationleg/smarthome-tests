7e7169d03f4a1f5d33703a3b2c18848a38383cc3
Krzysztof <krzysztof_rrr@wp.pl>
2019-07-11 00:45:30 +0200

    Started test file for openhab started features

--- molecule/default/tests/test_openhab_restore.py
+++ molecule/default/tests/test_openhab_restore.py
@@ -1,11 +1,12 @@
 import os
-
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('openhab')
 
 
+@pytest.mark.skip
 def test_openhab2_backup_archive_copied(host):
     f = host.file("/tmp/openhab2-backup.zip")
     assert f.exists

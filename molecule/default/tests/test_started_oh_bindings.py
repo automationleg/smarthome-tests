7f95b6a6b0318fbac2e7f176018bb6adf0925dd6
Krzysztof <krzysztof_rrr@wp.pl>
2019-07-22 01:02:50 +0200

    fix test_started_oh_bindings tests

--- molecule/default/tests/test_started_oh_bindings.py
+++ molecule/default/tests/test_started_oh_bindings.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import testinfra.utils.ansible_runner
 
@@ -11,18 +12,23 @@ def test_all_openhab_bindings_are_started(host, request):
     with open(path+'/openhab_features.list') as f:
         features = f.read()
    
-    started_features = []
-    for line in features.splitlines():
-        started_features.append(line.split()[0])
+    expected_started_features = [
+        line.split()[0]
+        for line in features.splitlines()
+    ]
 
     cmd = 'ssh -p 8101 openhab@localhost "feature:list|grep -v Uninstalled |grep ^openhab"'
-    active_features = host.command(cmd).stdout
+    response = host.command(cmd).stdout
+
+    # remove strange utf8 characters from the ssh response
+    ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
+    active_features = ansi_escape.sub('', response)
+
     not_started_features = [
         feature
-        for feature in started_features
+        for feature in expected_started_features
         if feature not in active_features
     ]
     
-    assert not_started_features, 'Not all openhab features started. features not started: '+not_started_features
-
+    assert len(not_started_features) == 0, 'Not all openhab features started. features not started: {}'.format(not_started_features)
     
